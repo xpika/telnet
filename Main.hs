4e2043151b727bce5a822632b385799c50303a01
root <hawk.alan@gmail.com>
2016-04-02 09:14:01 +0000

    updated

--- Main.hs
+++ Main.hs
@@ -2,15 +2,41 @@ module Main where
 import Network
 import Network.Socket
 import System.IO
+import System.Environment
+import Control.Monad
+import Control.Concurrent
 
 main :: IO ()
 main = withSocketsDo $ do
- addrInfos <- getAddrInfo Nothing (Just "127.0.0.1") (Just "4243")
+ args <- getArgs 
+ if args == [] then 
+   putStrLn "Usage: ip port"
+ else
+   rest args
+
+rest args = do
+ let portarg = drop 1 args
+     port = head $ (drop 1 args) ++ ["21"]
+     ip = head args
+ when (portarg == []) (putStrLn "using port 21")
+ addrInfos <- getAddrInfo Nothing (Just ip) (Just port)
  s <- socket (addrFamily (head addrInfos)) Stream defaultProtocol
  setSocketOption s KeepAlive 1
  connect s (addrAddress (head addrInfos))
  h <- socketToHandle s ReadWriteMode
- hSetBuffering h (BlockBuffering Nothing)
- let loop = do
-   l <- hGetLine h
-   putStrLn l
+ hSetBuffering h LineBuffering
+ hSetBuffering stdout LineBuffering
+ let loop = do {
+   ;y <- getLine
+   ;hPutStrLn h y
+   ;hFlush h
+   ;loop
+   }
+  in forkIO $ loop
+ let loop = do {
+   ;l <- hGetLine h
+   ;putStrLn l
+   ;loop
+   }
+  in loop
+ return ()
