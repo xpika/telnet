432be483831421d577a158f2466b296c541aa703
root <hawk.alan@gmail.com>
2016-04-04 09:49:13 +0000

    simplified test

--- Test.hs
+++ Test.hs
@@ -5,7 +5,7 @@ import Data.Char
 import TestEchoServer (testEchoServer)
 
 main = do    
-       let v1 = "hello"
+       let v1 = "HELLO"
        v2 <- testEchoServer "hello"
        defaultMain (tests (v1,v2))
 
@@ -13,5 +13,5 @@ tests vs = testGroup "Tests" [unitTests vs ]
 
 unitTests (v1,v2) = 
   testGroup "Unit tests"
-  [ testCase "List comparison (different length)" (assertEqual "test uppercase" (map toUpper v1) v2) ]
+  [ testCase "List comparison (different length)" (assertEqual "test uppercase" v1 v2) ]
 
